7a8a351ae91731516bd131244483eb03bc32053c
Mihai Voinea <hi@voinea.me>
2019-07-29 20:44:28 +0300

    Update request.js

--- src/request.js
+++ src/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./store";
 
 // API URL
-axios.defaults.baseURL = process.env.API_URL;
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost/" : "https://api.bookswap.ro";
 
 // Set JWT token header if token is set inside the state
 if (store.state.token !== undefined)
